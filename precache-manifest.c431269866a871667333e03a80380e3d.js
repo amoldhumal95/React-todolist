ef8c0b3ad900a7c694b434106a1f7d25b2d8b902
Amol Dhumal <amoldhumal95@gmail.com>
2020-09-14 19:54:04 +0530

    Updates

---
+++ precache-manifest.c431269866a871667333e03a80380e3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e443eddeadd4304ef0dc24de57a50e2",
+    "url": "/React-todolist/index.html"
+  },
+  {
+    "revision": "ea7804c7a22ca4e1b304",
+    "url": "/React-todolist/static/css/main.d61c9266.chunk.css"
+  },
+  {
+    "revision": "42001a3d17e6d25e071a",
+    "url": "/React-todolist/static/js/2.5f9c20fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todolist/static/js/2.5f9c20fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea7804c7a22ca4e1b304",
+    "url": "/React-todolist/static/js/main.fe01ffc1.chunk.js"
+  },
+  {
+    "revision": "8db25b14a419f8e53300",
+    "url": "/React-todolist/static/js/runtime-main.cfa4c84b.js"
+  }
+]);
